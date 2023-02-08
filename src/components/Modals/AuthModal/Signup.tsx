a7c02394c557a87a4099a8e66e94d3af94a58230
Peter Steinberg <bklynpeter@gmail.com>
2023-02-07 19:19:51 -0500

    working on connecting to Firebase

--- src/components/Modals/AuthModal/Signup.tsx
+++ src/components/Modals/AuthModal/Signup.tsx
@@ -2,22 +2,46 @@ import { Input, Button, Flex, Text } from '@chakra-ui/react'
 import { FC, useState, ChangeEvent, FormEvent } from 'react'
 import { authModalState, AuthModalState } from '@/atoms/authModalAtom'
 import { useSetRecoilState } from 'recoil'
+import { auth } from '@/firebase/clientApp'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 
 const Signup: FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState)
 
-  const [signupForm, setSignupForm] = useState({
+  const signupFormDefault = {
     email: '',
     password: '',
     confirmPassword: '',
-  })
+  }
+
+  const [signupForm, setSignupForm] = useState(signupFormDefault)
+
+  const [error, setError] = useState('')
+
+  const { email, password, confirmPassword } = signupForm
+
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    userError,
+  ] = useCreateUserWithEmailAndPassword(auth)
+
+  //Firebase logic
+  const submitForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (error) setError('')
+    if (password !== confirmPassword) {
+      setError('Passwords do not match')
+      return
+    }
+    createUserWithEmailAndPassword(email, password)
+    setSignupForm(signupFormDefault)
+    setAuthModalState(prev => ({ ...prev, isOpen: false }))
+  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSignupForm(p => ({ ...p, [e.target.name]: e.target.value }))
-  }
-  //Firebase logic
-  const submitForm = (e: FormEvent) => {
-    e.preventDefault()
   }
 
   const switchToLogin = () => {
@@ -32,7 +56,7 @@ const Signup: FC = () => {
         placeholder="email"
         type="email"
         mb={2}
-        value={signupForm.email}
+        value={email}
         onChange={handleChange}
         fontSize="10pt"
         bg="gray.50"
@@ -57,7 +81,7 @@ const Signup: FC = () => {
         placeholder="password"
         type="password"
         mb={2}
-        value={signupForm.password}
+        value={password}
         onChange={handleChange}
         fontSize="10pt"
         bg="gray.50"
@@ -82,7 +106,7 @@ const Signup: FC = () => {
         placeholder="confirm password"
         type="password"
         mb={2}
-        value={signupForm.confirmPassword}
+        value={confirmPassword}
         onChange={handleChange}
         fontSize="10pt"
         bg="gray.50"
@@ -101,7 +125,18 @@ const Signup: FC = () => {
           borderColor: 'blue.500',
         }}
       />
-      <Button width="100%" height="36px" my={2} type="submit">
+      {error && (
+        <Text textAlign="center" color="red">
+          {error}
+        </Text>
+      )}
+      <Button
+        width="100%"
+        height="36px"
+        my={2}
+        type="submit"
+        isLoading={loading}
+      >
         Submit
       </Button>
       <Flex fontSize="9pt" justify="center">
