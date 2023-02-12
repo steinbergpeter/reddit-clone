e77454cd09584af1d25cb25a3f83718505bad04a
Peter Steinberg <bklynpeter@gmail.com>
2023-02-12 12:45:18 -0500

    SSPages cannot "hide behind" index files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
-import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import ourTheme from '@/Styles/chakra/ourTheme';
-import Layout from '@/components/layouts/Layout';
-import { RecoilRoot } from 'recoil';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<RecoilRoot>
-		 <ChakraProvider theme={ourTheme}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		 </ChakraProvider>
-		</RecoilRoot>
-	);
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import ourTheme from "../Styles/chakra/ourTheme";
+import Layout from "../components/layouts/Layout";
+import "../firebase/clientApp";
+// import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <ChakraProvider theme={ourTheme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
+
+export default MyApp;
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,2 +1,68 @@
-import CommunityPage from './CommunityPage'
+import { communitiesState, Community } from '@/atoms/communitiesAtom'
+import CommunityNotFound from '@/components/Community/CommunityNotFound'
+import { auth, firestore } from '@/firebase/clientApp'
+import { doc, getDoc } from 'firebase/firestore'
+import { GetServerSidePropsContext, NextPage } from 'next'
+import { useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRecoilState } from 'recoil'
+import safeJsonStringify from 'safe-json-stringify'
+interface CommunityPageProps {
+  communityData: Community
+}
+
+const CommunityPage: NextPage<CommunityPageProps> = ({ communityData }) => {
+  const [user, loadingUser] = useAuthState(auth)
+  const [communityStateValue, setCommunityStateValue] = useRecoilState(
+    communitiesState
+  )
+
+  useEffect(() => {
+    setCommunityStateValue(p => ({
+      ...p,
+      currentCommunity: communityData,
+    }))
+  }, [communityData])
+
+  if (!communityData) {
+    return <CommunityNotFound />
+  }
+
+  return (
+    <div>
+      <h5>
+        currentCommunity:{'  '}
+        {JSON.stringify(communityStateValue.currentCommunity.id)}
+      </h5>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  console.log('getServerSideProps called')
+  //get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      'communities',
+      context.query.communityId as string
+    )
+    const communityDoc = await getDoc(communityDocRef)
+
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+        ),
+      },
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log('SSR Error:', err)
+    } else {
+      console.log('unidentified error: ', err)
+    }
+  }
+}
+
 export default CommunityPage
